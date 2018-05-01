d89fbe6c28e2f2b3e28897053e024263f4e580a2
olimpia-bradatan <bradatanolimpia@gmail.com>
2018-05-01 13:38:24 +0300

    Medical prescription as a PDF

--- PC/PC/Controllers/ArchivedateController.cs
+++ PC/PC/Controllers/ArchivedateController.cs
@@ -7,9 +7,11 @@ using System.Web.Mvc;
 
 using iText.Kernel.Pdf;
 using iText.Layout;
+using iText.Pdfa;
 using iText.Layout.Element;
 using PC.Models;
 using System.IO;
+using System.Drawing;
 
 namespace PC.Controllers
 {
@@ -21,7 +23,7 @@ namespace PC.Controllers
         public ActionResult ConvertMedicalPrescription(int idmedicalPrescription)
         {
 
-            return View(db.medicalPrescriptions.Find(idmedicalPrescription));
+                return View(db.medicalPrescriptions.Find(idmedicalPrescription));
         }
 
         // POST: Archivedate/Convert
@@ -29,25 +31,46 @@ namespace PC.Controllers
         public ActionResult ConvertMedicalPrescription(int idmedicalPrescription, medicalPrescription m)
         {
             Appointment a = db.Appointments.Find(idmedicalPrescription);
+            m = db.medicalPrescriptions.Find(idmedicalPrescription);
+            Patient p = db.Patients.Find(a.cardNumber);
+            Medic mm = db.Medics.Find(p.idMedic);
+            
             try
-            {
+             {
                 var exportFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                var exportFile = System.IO.Path.Combine(exportFolder, a.cardNumber + ".pdf");
+                var exportFile = System.IO.Path.Combine(exportFolder, p.firstName + " " + p.lastName + "." + a.cardNumber + ".pdf");
 
                 using (var writer = new PdfWriter(exportFile))
                 {
                     using (var pdf = new PdfDocument(writer))
                     {
                         var doc = new Document(pdf);
-                        doc.Add(new Paragraph("DIAGNOSTIC "));
-                        doc.Add(new Paragraph(m.Diagnostic));
-                        doc.Add(new Paragraph("MEDICATION "));
-                        doc.Add(new Paragraph(m.Medication));
-                        doc.Add(new Paragraph("FREE "));
+                        Table t = new Table(2);
+                        Paragraph para = new Paragraph("Medical prescription");
+                        para.SetBold();
+                        para.SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
+                        doc.Add(para);
+
+                        doc.Add(new Paragraph(""));
+                        doc.Add(new Paragraph("Patient first name: " + p.firstName));
+                        doc.Add(new Paragraph("Patient last name: " + p.lastName));
+                        doc.Add(new Paragraph("Card number: " + p.cardNumber));
+                        doc.Add(new Paragraph("Birth date: " + p.birthDate));
+
+                        t.AddCell("Medic name");
+                        t.AddCell(mm.lastName + " " + mm.firstName);
+                        t.AddCell("Appointment date");
+                        t.AddCell(""+a.Date);
+                        t.AddCell("DIAGNOSTIC");
+                        t.AddCell(m.Diagnostic);
+                        t.AddCell("MEDICATION");
+                        t.AddCell(m.Medication);
+                        t.AddCell("FREE");
                         if (m.Free == true)
-                            doc.Add(new Paragraph("YES"));
+                            t.AddCell("YES");
                         else
-                            doc.Add(new Paragraph("NO"));
+                            t.AddCell("NO");
+                        doc.Add(t);
                     }
                 }
                 // TODO: Add insert logic here
