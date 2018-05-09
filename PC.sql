10e2468c33e3bb7c80c5e498bd9ab0f7e2aa918f
Ahmad <noaa322@gmail.com>
2018-05-09 14:45:32 +0300

    added script to populate

---
+++ PC.sql
@@ -0,0 +1,288 @@
+--Populating Medic
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Anghel', 'Negoita', 'anghel_negoita');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Ciprian', 'Pop', 'cipri_pop');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Vasile', 'Caravana', 'vasille');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Gheorghe', 'Lazar', 'ghe-lazar');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Bogdan', 'Olariu', 'bogdanel25');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Calin', 'Vinter', 'vinterr42');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Cristian', 'Oprisan', 'Cryssty95');
+
+--Populating Assistant
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Olimpia', 'Bradatan', 5, 'oli_brada');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Cristina', 'Pop', 7, 'crispop84');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Ioana', 'Navodar', 1, 'ioana_navo23');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Iulia', 'Slavu', 2, 'slavuiulia_95');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Ovidiu', 'Olariu', 3, 'oolariu');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Horia', 'Brenciu', 4, 'brenciuu');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Marius', 'Nastasiu', 6, 'marius29');
+
+--Populating Admin
+
+INSERT INTO [dbo].[Admin]
+VALUES ('adminadmin',null);
+
+--Populating medicalPrescription
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Pneumonie', 'Antibotic', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Gripa cu tuse seaca', 'Paracetamol 2/zi + sirop Eurespal', 1);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Imunitate scazuta', 'Vitamine + Cetebe', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Alergie la praf', 'Aerius 1/zi', 1);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Febra si dureri musculare', 'Masaj Fastum Gel 3/zi', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Herpes', 'SystemWell 2/zi', 0);
+
+--Populating medicalRecord
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2015-04-04', 'Hepatita, Rujeola', 'Hipertensiune arteriala', 'Rujeola, Oreon', 'Aspacardin 1/zi', 'lipsa', '2016-03-09', 'lipsa');
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2017-11-12', 'Varicela, Rujeola', 'Hipertensiune arteriala', 'Rujeola', 'lipsa', 'lipsa', '2018-01-05', 'lipsa');
+
+--Populating Medic
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Anghel', 'Negoita', 'anghel_negoita');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Ciprian', 'Pop', 'cipri_pop');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Vasile', 'Caravana', 'vasille');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Gheorghe', 'Lazar', 'ghe-lazar');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Bogdan', 'Olariu', 'bogdanel25');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Calin', 'Vinter', 'vinterr42');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Cristian', 'Oprisan', 'Cryssty95');
+
+--Populating Assistant
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Olimpia', 'Bradatan', 5, 'oli_brada');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Cristina', 'Pop', 7, 'crispop84');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Ioana', 'Navodar', 1, 'ioana_navo23');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Iulia', 'Slavu', 2, 'slavuiulia_95');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Ovidiu', 'Olariu', 3, 'oolariu');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Horia', 'Brenciu', 4, 'brenciuu');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Marius', 'Nastasiu', 6, 'marius29');
+
+--Populating Admin
+
+INSERT INTO [dbo].[Admin]
+VALUES ('adminadmin',null);
+
+--Populating medicalPrescription
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Pneumonie', 'Antibotic', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Gripa cu tuse seaca', 'Paracetamol 2/zi + sirop Eurespal', 1);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Imunitate scazuta', 'Vitamine + Cetebe', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Alergie la praf', 'Aerius 1/zi', 1);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Febra si dureri musculare', 'Masaj Fastum Gel 3/zi', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Herpes', 'SystemWell 2/zi', 0);
+
+--Populating medicalRecord
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2015-04-04', 'Hepatita, Rujeola', 'Hipertensiune arteriala', 'Rujeola, Oreon', 'Aspacardin 1/zi', 'lipsa', '2016-03-09', 'lipsa');
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2017-11-12', 'Varicela, Rujeola', 'Hipertensiune arteriala', 'Rujeola', 'lipsa', 'lipsa', '2018-01-05', 'lipsa');
+
+--Populating Medic
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Anghel', 'Negoita', 'anghel_negoita');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Ciprian', 'Pop', 'cipri_pop');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Vasile', 'Caravana', 'vasille');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Gheorghe', 'Lazar', 'ghe-lazar');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Bogdan', 'Olariu', 'bogdanel25');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Calin', 'Vinter', 'vinterr42');
+
+INSERT INTO [dbo].[Medic]
+VALUES ('Cristian', 'Oprisan', 'Cryssty95');
+
+--Populating Assistant
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Olimpia', 'Bradatan', 5, 'oli_brada');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Cristina', 'Pop', 7, 'crispop84');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Ioana', 'Navodar', 1, 'ioana_navo23');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Iulia', 'Slavu', 2, 'slavuiulia_95');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Ovidiu', 'Olariu', 3, 'oolariu');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Horia', 'Brenciu', 4, 'brenciuu');
+
+INSERT INTO [dbo].[Assistant]
+VALUES ('Marius', 'Nastasiu', 6, 'marius29');
+
+--Populating Admin
+
+INSERT INTO [dbo].[Admin]
+VALUES ('adminadmin',null);
+
+--Populating medicalPrescription
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Pneumonie', 'Antibotic', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Gripa cu tuse seaca', 'Paracetamol 2/zi + sirop Eurespal', 1);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Imunitate scazuta', 'Vitamine + Cetebe', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Alergie la praf', 'Aerius 1/zi', 1);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Febra si dureri musculare', 'Masaj Fastum Gel 3/zi', 0);
+
+INSERT INTO [dbo].[medicalPrescription]
+VALUES ('Herpes', 'SystemWell 2/zi', 0);
+
+--Populating medicalRecord
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2015-04-04', 'Hepatita, Rujeola', 'Hipertensiune arteriala', 'Rujeola, Oreion', 'Aspacardin 1/zi', 'lipsa', '2016-03-09', 'lipsa');
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2017-11-12', 'Varicela, Rujeola', 'Hipertensiune arteriala', 'Rujeola', 'lipsa', 'lipsa', '2018-01-05', 'lipsa');
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2014-09-25', 'Rubeola, Varicela, Rujeola', 'Diabet', 'Rujeola, Varicela, Rubeola', 'lipsa', 'lipsa', '2012-05-25', 'lipsa');
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2011-06-16', 'Varicela, Tetanos', 'lipsa boli curente', 'Tahicardie, Deficit de iod', 'lipsa', 'Alergie la alimente dulci', '2018-01-05', 'lipsa');
+
+INSERT INTO [dbo].[medicalRecord]
+VALUES ('2017-11-12', 'Tetanos, Oreion', 'Astm bronsic', 'Rujeola, Varicela, Boala renala cronica', 'lipsa', 'lipsa', '2016-02-03', 'lipsa');
+
+--Populating Patient
+
+INSERT INTO [dbo].[Patient]
+VALUES ('28584970695048327384', 'Cipri', 'Nistor', '1992-08-22', 'cipri@gmail.com', 4, '1920822342673', 3);
+
+INSERT INTO [dbo].[Patient]
+VALUES ('18582742104908327384', 'Iulia', 'Albu', '1983-11-21', 'aiulia@gmail.com', 6, '1920822342673', 5);
+
+INSERT INTO [dbo].[Patient]
+VALUES ('84724949380048382734', 'Alexandra', 'Costea', '1998-03-01', 'calexandra@gmail.com', 2, '1920822342673', 1);
+
+INSERT INTO [dbo].[Patient]
+VALUES ('95048327384285849706', 'Ahmad', 'Bayan', '1995-11-12', 'noaa322@gmail.com', 1, '1951112160098', 2);
+
+INSERT INTO [dbo].[Patient]
+VALUES ('95085828374657289706', 'Marian-Valer', 'Bolintineanu', '1997-06-05', 'bolintineanuvaler@gmail.com', 3, '1970605303722', 4);
+
+INSERT INTO [dbo].[Patient]
+VALUES ('95085828373657289706', 'Robert', 'Corman', '1996-09-08', 'robert.corman89@gmail.com', 4, '1960908071970', 4);
+
+--Populating Appointment
+
+INSERT INTO [dbo].[Appointment]
+VALUES (1,'95085828373657289706', '2008-09-08', '16:00:00', 4);
+
+INSERT INTO [dbo].[Appointment]
+VALUES (3,'28584970695048327384', '2018-09-08', '12:00:00', 2);
+
+INSERT INTO [dbo].[Appointment]
+VALUES (2,'95085828374657289706', '2018-04-08', '09:30:00', 5);
+
+INSERT INTO [dbo].[Appointment]
+VALUES (2,'95048327384285849706', '2018-04-04', '15:30:00', 1);
+
+INSERT INTO [dbo].[Appointment]
+VALUES (5,'84724949380048382734', '2018-07-04', '11:45:00', 3);
+
+INSERT INTO [dbo].[Appointment]
+VALUES (4,'18582742104908327384', '2018-07-09', '17:15:00', 4);
+
+INSERT INTO [dbo].[Appointment]
+VALUES (4,'18582742104908327384', '2018-07-09', '17:15:00', 4);
