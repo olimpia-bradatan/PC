19ce13160202f88be4db24c451782af3c9813e4f
olimpia-bradatan <bradatanolimpia@gmail.com>
2018-05-13 17:44:36 +0300

    Added verification for appointment available time

--- PC/PC/Controllers/AppointmentController.cs
+++ PC/PC/Controllers/AppointmentController.cs
@@ -55,8 +55,35 @@ namespace PC.Controllers
                 }
                 else
                 {
-                    TempData["Warning"] = "Appointment already booked!";
-                    return RedirectToAction("AppointmentCreate");
+                    String[] availableHours = new String[] { "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30" };
+
+                    TimeSpan time = (TimeSpan)appointment.Time;
+                    String[] notAvailableHours = new String[20];
+                    int k = 0;
+                    if (db.Appointments.Count() > 0)
+                    {
+                        foreach (var a in db.Appointments)
+                        {
+                            if (a.Date.Equals(appointment.Date))
+                            {
+                                TimeSpan time1 = (TimeSpan)a.Time;
+                                notAvailableHours[k++] = time1.ToString().Split(':')[0] + ":" + time1.ToString().Split(':')[1];
+                            }
+                        }
+                    }
+                    String content = "";
+                    int ok1;
+                    for (int i = 0; i < availableHours.Length; i++)
+                    {
+                        ok1 = 1;
+                        for (int j = 0; j < k; j++)
+                            if (notAvailableHours[j].Equals(availableHours[i]))
+                                ok1 = 0;
+                        if (ok1 == 1)
+                            content = content + availableHours[i] + " | ";
+                        
+                    }
+                    TempData["Warning"] = "Appointment already booked! Times available this day are: " + content;
                 }
             }
             return View();
